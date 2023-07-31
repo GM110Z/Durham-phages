48d8a66249928625f8ca3285d4abf7306cf40a5d
GM110Z <72254136+GM110Z@users.noreply.github.com>
2023-07-31 22:07:39 +0100

    Create mash_and_taxonomy.sh

---
+++ mash_and_taxonomy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VAR=$(ls *.fasta)
+
+for file in ${VAR}
+   do
+      mash sketch -o ${file}sketch ${file}
+      mash dist  ${file}sketch.msh refseq.genomes.k21s1000.msh -d 0.04 >> ${file}.tab
+      cut -d$'\t' -f 2 ${file}.tab |cut -d '_' -f1,2 >> ${file}_ids.tab
+   done
+
+VAR2=$(ls *_ids.tab)
+#need the taxo_loop.sh in same directory
+for i in ${VAR2}
+   do
+      ./taxo_loop.sh ${i}
+   done
+
